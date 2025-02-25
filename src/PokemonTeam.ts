f52b78f2d3ab50b97547971555065f0c7bba4b75
Jonatan <jonatanav255@hotmail.com>
2025-02-24 21:34:22 -0600

    adding register Pokemon and ExistsPokemonByID

--- src/PokemonTeam.ts
+++ src/PokemonTeam.ts
@@ -2,4 +2,38 @@ import { Pokemon } from './Pokemon'
 
 export class PokemonTeam {
   team: Pokemon[] = []
+
+  private existsPokemonByID (_id: number): boolean {
+    return this.team.some(pokemon => pokemon.id === _id)
+  }
+
+  public registerPokemon (
+    _id: number,
+    _name: string,
+    _type: string,
+    _powerLevel: number
+  ): boolean {
+    // Implementation
+    if (this.existsPokemonByID(_id)) {
+      throw new Error('The ID already exists')
+    }
+
+    if (!_name.trim()) {
+      throw new Error('Name Cannot be empty')
+    }
+
+    if (_powerLevel <= 1 || _powerLevel >= 100) {
+      throw new Error('Power level should be between 1 and 100')
+    }
+
+    if (_id < 100000 || _id > 999999) {
+      throw new Error('El ID should have 6 numbers (ej. 100000 - 999999).')
+    }
+
+    const newPokemon = new Pokemon(_id, _name, _type, _powerLevel)
+
+    this.team.push(newPokemon)
+
+    return true
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,19 @@ import { Pokemon } from './Pokemon'
 import { PokemonTeam } from './PokemonTeam'
 
 const pokemonTeam = new PokemonTeam()
-const pikachu = new Pokemon(1, "pikachu", "Fire", 50)
 
-console.log(pikachu.name = "change")
-// console.log(pokemonTeam)
+try {
+  if (pokemonTeam.registerPokemon(100001, 'Pikachu', 'Electric', 50)) {
+    console.log('Pokemon Pikachu was created successfully.')
+  }
+  if (pokemonTeam.registerPokemon(100002, 'Charmander', 'Fire', 60)) {
+    console.log('Pokemon Charmander was created successfully.')
+  }
+  if (pokemonTeam.registerPokemon(100003, 'Bulbasaur', 'Grass', 55)) {
+    console.log('Pokemon Bulbasaur was created successfully.')
+  }
+} catch (error) {
+  console.error('Error registering Pokemon:', error)
+}
+
+console.log(pokemonTeam)
